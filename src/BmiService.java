af1eaee5fedb4bea43239c39781f874bf3396b9f
Dolgov David <dolgov.david.1993@gmail.com>
2023-03-10 18:18:39 +0300

    fixed errors

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,8 +1,8 @@
 public class BmiService {
 
-    public float calculate(float kg, float cm) {
-        float x=(cm/100)*(cm/100);                       // для формулы переводим в метры и возводим в квадрат
-        float result=kg/x;                              // формула ИМТ киллограммы делённые на метры в квадрате.
+    public float calculate(float kilograms, float centimeters) {
+        float meters = (centimeters / 100) * (centimeters / 100);        // для формулы переводим в метры и возводим в квадрат
+        float result = kilograms / meters;                              // формула ИМТ киллограммы делённые на метры в квадрате.
         return result;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,31 +2,31 @@ public class Main {
     public static void main(String[] args) {
 
         BmiService service = new BmiService();
-        float weight=102.3F;                            // вес в киллограммах
-        float growth=186.45F;                           // рост в сантиметрах
-        float bMi= service.calculate(weight,growth);
+        float weight = 100F;                            // вес в киллограммах
+        float growth = 186F;                           // рост в сантиметрах
+        float bMassIndex = service.calculate(weight, growth);
 
         System.out.println();
-        System.out.println("Ваш индекс массы тела равен "+bMi);
+        System.out.println("Ваш индекс массы тела равен " + bMassIndex);
         System.out.println();
         System.out.println("Интерпритация индекса массы тела");
 
-        if(bMi<18.5) {
+        if (bMassIndex < 18.5) {
             System.out.println("ИМТ < 18.5: Ниже нормального веса");
-        }else {
-            if (18.5<=bMi & bMi<25) {
+        } else {
+            if (18.5 <= bMassIndex & bMassIndex < 25) {
                 System.out.println("ИМТ >= 18.5 И < 25: Нормальный вес");
-            }else {
-                if (25<=bMi & bMi<30) {
+            } else {
+                if (25 <= bMassIndex & bMassIndex < 30) {
                     System.out.println("ИМТ >= 25 И < 30: Избыточный вес");
-                }else {
-                    if (30<=bMi & bMi<35){
+                } else {
+                    if (30 <= bMassIndex & bMassIndex < 35) {
                         System.out.println("ИМТ >= 30 И < 35: Ожирение 1 степени");
-                    }else {
-                        if(35<=bMi & bMi<40){
+                    } else {
+                        if (35 <= bMassIndex & bMassIndex < 40) {
                             System.out.println("ИМТ >= 35 И < 40: Ожирение 2 степени");
-                        }else {
-                            if (35<=bMi){
+                        } else {
+                            if (35 <= bMassIndex) {
                                 System.out.println("ИМТ >= 40: Ожирение 3 степени");
                             }
                         }
